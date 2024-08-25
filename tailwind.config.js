6a8e8de26906acfc2374702c58dbaee99736d1a2
Amritha043 <amrithaa043@gmail.com>
2024-08-26 04:04:43 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellow: {
+          100: '#FFF9C4',
+          200: '#FFF59D',
+          300: '#FFF176',
+          400: '#FDD835',
+          500: '#FBC02D',
+          600: '#F9A825',
+          700: '#F57F17',
+          800: '#F57F17',
+          900: '#FF6F00',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
